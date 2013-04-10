fe2e32c980e9b80e93e2b6403d639ce308e5304d
Brendan Forster <me@brendanforster.com>
2013-04-11 09:47:14 +1200

    added the default RESX file because i'm sick of seeing it with a compile issue

--- src/Code52.i18n.MVC/NuSpec/tools/install.ps1
+++ src/Code52.i18n.MVC/NuSpec/tools/install.ps1
@@ -1,4 +1,12 @@
 param($installPath, $toolsPath, $package, $project)
+
+$buildProject = @([Microsoft.Build.Evaluation.ProjectCollection]::GlobalProjectCollection.GetLoadedProjects($project.FullName))[0]
+
+$toEdit = $buildProject.Xml.Items | where-object { $_.Include -eq "Resources\Language.Designer.cs" }
+$toEdit.AddMetaData("DependentUpon", "Resources\Language.resx")
+
+$project.Save()
+
 $path = [System.IO.Path]
 $readmefile = $path::Combine($path::GetDirectoryName($project.FileName), "App_Readme\Code52.i18n.MVC.readme.txt")
 $DTE.ItemOperations.OpenFile($readmefile)
